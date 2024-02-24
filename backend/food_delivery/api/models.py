a55b9f7a8f869001f8685ee6147a2042b74dc2fa
Yakobus Iryanto Prasethio <yakobusiryantoprasethio@gmail.com>
2024-02-24 19:55:08 +0700

    feat: redo init migrations to fit schema

--- backend/food_delivery/api/models.py
+++ backend/food_delivery/api/models.py
@@ -1,18 +1,22 @@
 from django.db import models
-from django.contrib.auth.models import User
+from django.contrib.auth.models import AbstractUser
 from django.core.validators import MinValueValidator, MaxValueValidator
 
 # Create your models here.
-class UserProfile(models.Model):
+class CustomUser(AbstractUser):
     class Role(models.TextChoices):
         CUSTOMER = "CUSTOMER", "Customer"
         RESTAURANT = "RESTAURANT", "Restaurant"
         DELIVERY = "DELIVERY", "Delivery"
 
-    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    email = models.EmailField(unique=True)
     role = models.CharField(max_length=10, choices=Role.choices, default=Role.CUSTOMER)
-    address = models.TextField(null=True)
     phone_number = models.CharField(max_length=20)
+
+class CustomerProfile(models.Model):
+    user = models.OneToOneField(CustomUser, on_delete=models.CASCADE)
+    address = models.TextField(null=True, blank=True)
+    image_path = models.ImageField(null=True, blank=True)
 
 class DeliveryProfile(models.Model):
     class Availability(models.TextChoices):
@@ -20,20 +24,21 @@ class DeliveryProfile(models.Model):
         BUSY = "BUSY", "Busy"
         OFF = "OFF", "Off"
 
-    profile = models.OneToOneField(UserProfile, on_delete=models.CASCADE)
+    user = models.OneToOneField(CustomUser, on_delete=models.CASCADE)
     availability = models.TextField(choices=Availability.choices, default=Availability.AVAILABLE)
 
 class Restaurant(models.Model):
     name = models.CharField(max_length=100)
     address = models.TextField()
     phone_number = models.CharField(max_length=20)
-    email = models.EmailField()
+    email = models.EmailField(unique=True)
     rating = models.FloatField(
         default=5.0, 
         validators=[MinValueValidator(0.0), MaxValueValidator(5.0)])
     delivery_radius = models.IntegerField(validators=[MinValueValidator(1)])
     opening_time = models.TimeField()
     closing_time = models.TimeField()
+    image_path = models.ImageField(null=True, blank=True)
 
 class Menu(models.Model):
     class FoodCategory(models.TextChoices):
@@ -50,10 +55,10 @@ class Menu(models.Model):
     
     restaurant = models.ForeignKey(Restaurant, related_name="restaurant_menu", on_delete=models.CASCADE)
     name = models.CharField(max_length=100)
-    description = models.TextField(null=True)
+    description = models.TextField(null=True, blank=True)
     price = models.IntegerField()
-    category = models.TextField(choices=FoodCategory.choices, null=True)
-    image_path = models.ImageField()
+    category = models.CharField(max_length=20, choices=FoodCategory.choices, null=True, blank=True)
+    image_path = models.ImageField(null=True, blank=True)
 
 class Order(models.Model):
     class OrderStatus(models.TextChoices):
@@ -62,13 +67,13 @@ class Order(models.Model):
         OUT_FOR_DELIVERY = "OUT_FOR_DELIVERY", "Out for Delivery" 
         DELIVERED = "DELIVERED", "Delivered" 
 
-    customer = models.ForeignKey(User, related_name="orders_placed", on_delete=models.CASCADE)
-    delivery_person = models.ForeignKey(User, null=True, related_name="deliveries", on_delete=models.CASCADE)
+    customer = models.ForeignKey(CustomUser, related_name="orders_placed", on_delete=models.CASCADE)
+    delivery_person = models.ForeignKey(CustomUser, null=True, related_name="deliveries", on_delete=models.CASCADE)
     restaurant = models.ForeignKey(Restaurant, related_name="placed_orders", on_delete=models.CASCADE)
-    status = models.TextField(choices=OrderStatus.choices, default=OrderStatus.PLACED)
+    status = models.CharField(max_length=20 ,choices=OrderStatus.choices, default=OrderStatus.PLACED)
     total_price = models.IntegerField()
     order_time = models.DateTimeField(auto_now_add=True)
-    delivery_time = models.DateTimeField(null=True)
+    delivery_time = models.DateTimeField(null=True, blank=True)
 
 class OrderItem(models.Model):
     order = models.ForeignKey(Order, on_delete=models.CASCADE)
