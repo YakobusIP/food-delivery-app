32eec41b250587852c14155625e006f66f73bb41
Yakobus Iryanto Prasethio <yakobusiryantoprasethio@gmail.com>
2024-02-24 09:20:38 +0700

    feat: first migration

--- backend/food_delivery/food_delivery/settings.py
+++ backend/food_delivery/food_delivery/settings.py
@@ -77,8 +77,12 @@ WSGI_APPLICATION = 'food_delivery.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'food_delivery',
+        'USER': 'postgres',
+        'PASSWORD': 'postgres',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
